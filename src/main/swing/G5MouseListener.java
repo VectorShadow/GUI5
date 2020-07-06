66b089a0bb6cd56b656c37674bd524f984b27c80
VectorShadow <ncbrayshaw@hotmail.com>
2020-07-06 21:25:03 +0430

    Edited a todo.

--- src/main/swing/G5MouseListener.java
+++ src/main/swing/G5MouseListener.java
@@ -3,8 +3,9 @@ package main.swing;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-//todo - lots of work here probably: we need to find where the mouse is, translate that to regional coordinates,
-// and report those coordinates along with the mouseEvent to the appropriate MouseInputHandler
+//todo - lots of work here probably: we need to find where the mouse is, rescale that position from the OutputPanel
+// Dimension to the Canvas Dimension, translate that to regional coordinates,
+// and report those coordinates along with the mouseEvent to the appropriate MouseInputHandler.
 public class G5MouseListener implements MouseListener {
     @Override
     public void mouseClicked(MouseEvent e) {
