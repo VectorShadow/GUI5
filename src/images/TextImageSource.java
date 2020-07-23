c19c2bf3c22424f4fcdc4eb1f7f75c71b4378029
VectorShadow <ncbrayshaw@hotmail.com>
2020-07-23 22:34:01 +0300

    Updated color scaling.

--- src/images/TextImageSource.java
+++ src/images/TextImageSource.java
@@ -6,7 +6,13 @@ import java.awt.image.BufferedImage;
 /**
  * Define an image source based on a symbol with foreground and background colors.
  */
+//todo - solve the rendering problem!
 public class TextImageSource extends ImageSource {
+
+//    private static final RenderingHints DISABLE_FRACTIONALMETRICS =
+//            new RenderingHints(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_OFF);
+//    private static final RenderingHints DISABLE_ANTI_ALIASING =
+//            new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
 
     private final Color background;
     private final Color foreground;
@@ -32,7 +38,9 @@ public class TextImageSource extends ImageSource {
         renderer.setForeground(foreground);
         renderer.setText("" + symbol);
         BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        renderer.paint(bi.getGraphics());
+        Graphics2D g2d = (Graphics2D)bi.getGraphics();
+        //g2d.setRenderingHints(DISABLE_FRACTIONALMETRICS);
+        renderer.paint(g2d);
         return bi;
     }
 
